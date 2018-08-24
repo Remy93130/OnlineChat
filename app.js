36024d147fcbcdf81aa6ebc634720d2b0008b600
Rémy Barberet <32952895+Remy93130@users.noreply.github.com>
2018-08-24 17:50:15 +0200

    openshift

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const app = express()
 const twig = require('twig')
 const path = require('path')
 
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || 'localhost'
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
 	res.render('index.twig', {name: 'remy'})
 })
 
-app.listen(8080, console.log('Listen port 8080'))
+app.listen(8080, () => {
+	console.log('Listening on ' + server_ip_address)
+	console.log('Port ' + server_port)
+})
